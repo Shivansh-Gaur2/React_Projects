5d2ce4adacb5d8af6f758e2d0984302c861c8b04
Shivansh Gaur <shivanshgaur.bt21ele@pec.edu.in>
2024-03-21 10:04:37 +0530

    Added SQL database

--- foodies_project/lib.js/meals.js
+++ foodies_project/lib.js/meals.js
@@ -1,29 +1,66 @@
-import sql from 'better-sqlite3';
+import fs from "node:fs";
 
-const db = sql('meals.db');
-// making db object by passing 
-// calling the function and passing 
+import sql from "better-sqlite3";
+import slugify from "slugify";
+import xss from "xss";
+
+const db = sql("meals.db");
+// making db object by passing
+// calling the function and passing
 // the name of the database file
 
-export async function getMeals(){
-    // async funtion will return a promise
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+export async function getMeals() {
+  // async funtion will return a promise
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 
-    // normally wont add this promise as it will slow data but added headers
-    // just for demo
+  // normally wont add this promise as it will slow data but added headers
+  // just for demo
 
-    // throw new Error('Could not fetch meals');
-     // to test the error page
+  // throw new Error('Could not fetch meals');
+  // to test the error page
 
-   return db.prepare('SELECT * FROM meals').all();
-//    prepare a new SQL statemanet where i want to call all 
-//    columns from the meals table
+  return db.prepare("SELECT * FROM meals").all();
+  //    prepare a new SQL statemanet where i want to call all
+  //    columns from the meals table
 
-// .run() is used when you want to add Data, .all() when you want to retrieve multiple RowingSharp
-// and .get() when you want to retrieve only a single row
+  // .run() is used when you want to add Data, .all() when you want to retrieve multiple RowingSharp
+  // and .get() when you want to retrieve only a single row
 }
 
-export function getMeal(slug){
-    return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
-    // add dynamic values by ? and adding the value of ? with .get() function
+export function getMeal(slug) {
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
+  // add dynamic values by ? and adding the value of ? with .get() function
 }
+
+export async function saveMeal(meal) {
+    meal.slug = slugify(meal.title, { lower: true });
+    meal.instructions = xss(meal.instructions);
+  
+    const extension = meal.image.name.split('.').pop();
+    const fileName = `${meal.slug}.${extension}`;
+  
+    const stream = fs.createWriteStream(`public/images/${fileName}`);
+    const bufferedImage = await meal.image.arrayBuffer();
+  
+    stream.write(Buffer.from(bufferedImage), (error) => {
+      if (error) {
+        throw new Error('Saving image failed!');
+      }
+    });
+  
+    meal.image = `/images/${fileName}`;
+  
+    db.prepare(`
+      INSERT INTO meals
+        (title, summary, instructions, creator, creator_email, image, slug)
+      VALUES (
+        @title,
+        @summary,
+        @instructions,
+        @creator,
+        @creator_email,
+        @image,
+        @slug
+      )
+    `).run(meal);
+  }
